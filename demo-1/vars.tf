a331e09e060632d44f8a5e95428e7ddc0276d9e0
Héctor Villarroel <hectorvillarroel.p@gmail.com>
2018-06-11 15:26:38 -0400

    Updated amis by region

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -6,8 +6,7 @@ variable "AMIS" {
   type = "map"
 
   default = {
-    us-east-1 = "ami-a4dc46db"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-c5cf55ba"
+    us-west-1 = "ami-7c938b1c"
   }
 }
