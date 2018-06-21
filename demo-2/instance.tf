21b4bb518dec8129c36b3a61cc2942ab2d02b58d
Héctor Villarroel <hectorvillarroel.p@gmail.com>
2018-06-21 16:38:10 -0400

    Added red for demo2

--- demo-2/instance.tf
+++ demo-2/instance.tf
@@ -1,25 +1,123 @@
-resource "aws_key_pair" "mykey" {
-  key_name = "mykey"
+resource "aws_key_pair" "test_terraform" {
+  key_name   = "test_terraform"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
 
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags {
+    Name = "main"
+  }
+}
+
+# Define the public subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = "${aws_vpc.terraform_vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "Web Public Subnet"
+  }
+}
+
+# Define the security group for public subnet
+resource "aws_security_group" "sgweb" {
+  name        = "vpc_test_web"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.terraform_vpc.id}"
+
+  tags {
+    Name = "Web Server SG"
+  }
+}
+
+# Define the internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.terraform_vpc.id}"
+
+  tags {
+    Name = "VPC IGW"
+  }
+}
+
+# Define the route table
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.terraform_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "Public Subnet RT"
+  }
+}
+
+# Assign the route table to the public Subnet
+resource "aws_route_table_association" "web-public-rt" {
+  subnet_id      = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
+}
+
+# Define webserver inside the public subnet
 resource "aws_instance" "example" {
-  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type = "t2.micro"
-  key_name = "${aws_key_pair.mykey.key_name}"
+  ami                         = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type               = "t2.micro"
+  key_name                    = "${aws_key_pair.test_terraform.key_name}"
+  subnet_id                   = "${aws_subnet.public-subnet.id}"
+  vpc_security_group_ids      = ["${aws_security_group.sgweb.id}"]
+  associate_public_ip_address = true
+
+  tags {
+    Name = "webserver"
+  }
 
   provisioner "file" {
-    source = "script.sh"
+    source      = "script.sh"
     destination = "/tmp/script.sh"
   }
+
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/script.sh",
-      "sudo /tmp/script.sh"
+      "sudo /tmp/script.sh",
     ]
   }
+
   connection {
-    user = "${var.INSTANCE_USERNAME}"
+    user        = "${var.INSTANCE_USERNAME}"
     private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
   }
 }
