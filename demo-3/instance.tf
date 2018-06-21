85225eff0b5ead542d8dcb24e6cebd0d03e3f797
Héctor Villarroel <hectorvillarroel.p@gmail.com>
2018-06-21 18:03:22 -0400

    Added red and added attribute associate_public_ip_address for get a public_ip and output

--- demo-3/instance.tf
+++ demo-3/instance.tf
@@ -1,10 +1,102 @@
-resource "aws_instance" "example" {
-  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type = "t2.micro"
-  provisioner "local-exec" {
-     command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
+resource "aws_key_pair" "test_terraform" {
+  key_name   = "test_terraform"
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+}
+
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags {
+    Name = "main"
   }
 }
+
+# Define the public subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = "${aws_vpc.terraform_vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "Web Public Subnet"
+  }
+}
+
+# Define the security group for public subnet
+resource "aws_security_group" "sgweb" {
+  name        = "vpc_test_web"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.terraform_vpc.id}"
+
+  tags {
+    Name = "Web Server SG"
+  }
+}
+
+# Define the internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.terraform_vpc.id}"
+
+  tags {
+    Name = "VPC IGW"
+  }
+}
+
+# Define the route table
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.terraform_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "Public Subnet RT"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                         = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${aws_subnet.public-subnet.id}"
+  vpc_security_group_ids      = ["${aws_security_group.sgweb.id}"]
+  associate_public_ip_address = true
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
+  }
+}
+
 output "ip" {
-    value = "${aws_instance.example.public_ip}"
+  value = "${aws_instance.example.public_ip}"
 }
